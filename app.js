bff00314d4b1b18a246e5feee5549126d19e116e
z1196 <zhaohuan.1196@gmail.com>
2026-05-06 17:34:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/", async (req, res) => {
     try {
         const browser = await puppeteer.launch({
             headless: true,
-            executablePath: "/usr/bin/google-chrome",   // 指定容器里的 Chrome
+            executablePath: "/usr/bin/google-chrome", 
             args: [
                 "--no-sandbox",
                 "--disable-setuid-sandbox",
