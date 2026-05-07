5cb0f0088693f9821b74eb2ef27d9dc2cc8e342d
z1196 <zhaohuan.1196@gmail.com>
2026-05-07 11:45:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-const express = require("express");
-const puppeteer = require("puppeteer-core");
+const express = require('express');
+const puppeteer = require('puppeteer-core');
 
 const app = express();
 const PORT = 51888;
 
-app.get("/parse", async (req, res) => {
+app.get('/parse', async (req, res) => {
   const targetUrl = req.query.url;
   if (!targetUrl) return res.send("missing url");
 
   try {
     const browser = await puppeteer.launch({
-      headless: false,
-      executablePath: "/usr/bin/chromium",
+      headless: "new",
+      executablePath: "/usr/bin/chromium-browser",
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
-        "--enable-webgl",
-        "--ignore-gpu-blocklist",
-        "--enable-features=NetworkService,NetworkServiceInProcess",
-        "--disable-blink-features=AutomationControlled",
-        "--allow-running-insecure-content",
-        "--disable-web-security",
-        "--disable-features=IsolateOrigins,site-per-process",
+        "--disable-gpu",
         "--disable-dev-shm-usage",
-        "--window-size=1280,800"
+        "--disable-blink-features=AutomationControlled"
       ]
     });
 
@@ -56,74 +50,13 @@ app.get("/parse", async (req, res) => {
       } catch {}
     });
 
-    await page.evaluateOnNewDocument(() => {
-      const origFetch = window.fetch;
-      window.fetch = async (...args) => {
-        const r = await origFetch(...args);
-        r.clone().text().then(t => {
-          const m = t.match(/https?:\/\/[^\s"'\\]+/);
-          if (m) window.__REAL_URL__ = m[0];
-        });
-        return r;
-      };
-    });
-
-    await page.evaluateOnNewDocument(() => {
-      const open = XMLHttpRequest.prototype.open;
-      XMLHttpRequest.prototype.open = function (...args) {
-        this.addEventListener("load", function () {
-          try {
-            const m = this.responseText.match(/https?:\/\/[^\s"'\\]+/);
-            if (m) window.__REAL_URL__ = m[0];
-          } catch {}
-        });
-        return open.apply(this, args);
-      };
-    });
-
-    await page.evaluateOnNewDocument(() => {
-      Object.defineProperty(HTMLMediaElement.prototype, "src", {
-        set(v) {
-          window.__REAL_URL__ = v;
-        }
-      });
-    });
-
-    await page.evaluateOnNewDocument(() => {
-      window.Hls = window.Hls || {};
-      const orig = window.Hls.loadSource;
-      window.Hls.loadSource = function (url) {
-        window.__REAL_URL__ = url;
-        return orig.call(this, url);
-      };
-    });
-
-    await page.evaluateOnNewDocument(() => {
-      Object.defineProperty(window, "__NUXT__", {
-        set(v) {
-          try {
-            const play = v?.state?.video?.playUrl;
-            if (play) window.__REAL_URL__ = play;
-          } catch {}
-          window.__NUXT___data = v;
-        },
-        get() {
-          return window.__NUXT___data;
-        }
-      });
-    });
-
     await page.goto(targetUrl, { waitUntil: "networkidle2", timeout: 30000 });
 
-    await page.waitForTimeout(6000);
-
-    const injected = await page.evaluate(() => window.__REAL_URL__);
+    await page.waitForTimeout(5000);
 
     await browser.close();
 
-    if (injected && !realUrl) realUrl = injected;
-
-    if (!realUrl) return res.send("cannot extract real url");
+    if (!realUrl) return res.send("cannot extract real url (headless mode)");
 
     return res.send(realUrl);
 
