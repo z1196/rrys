43fd774788b56d8242bf18facef9503facfd8261
z1196 <zhaohuan.1196@gmail.com>
2026-05-07 16:54:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# 宿主机挂载进来的 /opt/rrys 是空的 → 自动初始化 app.js
+# 如果宿主机挂载进来的 /opt/rrys 没有 app.js → 自动初始化
 if [ ! -f /opt/rrys/app.js ]; then
   echo "Initializing app.js..."
   cp /opt/rrys-template/app.js /opt/rrys/app.js
